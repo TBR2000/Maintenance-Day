e689ff9c59f10a60fd2e64f3f10a17ddbcb710e7
TBR2000 <timothyrabbidge@live.com.au>
2021-08-05 13:48:01 +1000

    anaylitics algo written and tested

---
+++ maintenance-day/server/utils/analytics.js
@@ -0,0 +1,26 @@
+const query = require('./querybuilder')
+
+const deviationLimit = 10;
+const value = Number(res.value)
+const setpoint = Number (res.setpoint)
+
+//Check if input is plus/minus x percent from setpoint
+const setpointDeviation = value / setpoint
+const xFromSetpoint = deviationLimit/100
+const lowerLimit = 1 - xFromSetpoint
+const upperLimit = 1 + xFromSetpoint
+const roundUp = Math.ceil(setpointDeviation*100)/100
+const roundDown = Math.floor(setpointDeviation*100)/100
+console.log(setpointDeviation)
+console.log(roundUp, roundDown)
+
+if (roundDown > upperLimit || roundUp < lowerLimit ){
+    console.log('issue')
+}
+
+
+//Check if anologue point is at 0% or 100%
+
+if(res.actuator === '0' || '100' ) {
+    console.log('issue')
+};
